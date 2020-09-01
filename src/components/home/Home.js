c8848a2ad2a7b99b996231b0f9916d1c12759aca
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2020-09-01 16:52:57 -0500

    Screen size detected. Rendering modifications for items

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,33 +1,72 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import styles from './home.module.css';
 import Filter from '../filter/Filter';
 import Search from '../search/Search';
 import Lister from '../lister/Lister';
 import Pages from '../pages/Pages';
 
-export default function Home() {
+export default class Home extends Component {
 
-    let currDate = new Date().toLocaleDateString()
+    constructor(props) {
+        super(props);
+        this.state = {
+            windowWidth: 0,
+            windowHeight: 0
+        };
+        this.wrapperRef = React.createRef();
+        this.updateDimensions = this.updateDimensions.bind(this);
+    }
 
-    return (
-        <div style={{ height: "100vh" }}>
-            <div className={styles.searcher}>
-                <Search />
-            </div>
-            <div className={styles.container}>
-                <div className={styles.side_filter}>
-                    <Filter />
+    handleClick() {
+        const wrapper = this.wrapperRef.current;
+        wrapper.classList.toggle('is-nav-open');
+    }
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+
+    updateDimensions() {
+        let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
+        let windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+
+        this.setState({ windowWidth, windowHeight });
+    }
+    currDate = {
+        date: new Date().toLocaleDateString()
+    }
+
+    render() {
+        const {windowWidth} = this.state;
+        
+        const isSmallScreen = windowWidth < 768;
+
+        return (
+            
+            <div style={{ height: "100vh" }}>
+                
+                <div className={styles.searcher} style={{"flex-direction": isSmallScreen ? "row": null}}>
+                    <Search />
                 </div>
-                <div className={styles.mainBlock}>
-                    <Lister />
-                    <Pages />
+                <div className={styles.container} >
+                    {isSmallScreen ? null : <div className={styles.side_filter} >
+                        <Filter />
+                    </div>}
+                    <div className={styles.mainBlock} style={{width: isSmallScreen ? "100%" : null}}>
+                        <Lister />
+                        <Pages />
+                    </div>
+                </div>
+                <div className={styles.signature} >
+                    <h3 > Juan J. Alarcon</h3>
+                    <h3> {this.currDate.date}</h3>
                 </div>
             </div>
-            <div className={styles.signature} >
-                <h3 > Juan J. Alarcon</h3>
-                <h3> {currDate}</h3>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
