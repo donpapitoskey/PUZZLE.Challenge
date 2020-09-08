90694ef035c1b8afb3466d24df73004a897e6bc8
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2020-09-08 18:53:54 -0500

    Updates

---
+++ precache-manifest.be8391a75298d73f75570b07c4a53639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbb2a57bfff343c2422f674b10b0049",
+    "url": "/PUZZLE.Challenge/index.html"
+  },
+  {
+    "revision": "8baf44ac0974f2e9e20d",
+    "url": "/PUZZLE.Challenge/static/css/main.0df577b6.chunk.css"
+  },
+  {
+    "revision": "a1542800f4b2560551f9",
+    "url": "/PUZZLE.Challenge/static/js/2.93aec2aa.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/PUZZLE.Challenge/static/js/2.93aec2aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8baf44ac0974f2e9e20d",
+    "url": "/PUZZLE.Challenge/static/js/main.7ba3e757.chunk.js"
+  },
+  {
+    "revision": "7bda71d2980c905619a7",
+    "url": "/PUZZLE.Challenge/static/js/runtime-main.52295115.js"
+  }
+]);
--- src/services/apollo.js
+++
@@ -1,7 +0,0 @@
-import ApolloClient from 'apollo-boost';
-
-let client = new ApolloClient({ 
-    uri: "https://rickandmortyapi.com/graphql"
-});
-
-export default client;
